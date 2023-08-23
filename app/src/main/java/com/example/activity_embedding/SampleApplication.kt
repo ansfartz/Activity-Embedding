242c3e454ae3f7ea730ddead94c30c0f82baffd0
Andrei Sfartz <andrei.sfartz@microsoft.com>
2023-08-23 11:42:15 +0300

    sample implementation using an xml config file

---
+++ app/src/main/java/com/example/activity_embedding/SampleApplication.kt
@@ -0,0 +1,13 @@
+package com.example.activity_embedding
+
+import android.app.Application
+import androidx.window.embedding.RuleController
+
+class SampleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        RuleController.getInstance(this)
+            .setRules(RuleController.parseRules(this, R.xml.main_split_config))
+    }
+}
